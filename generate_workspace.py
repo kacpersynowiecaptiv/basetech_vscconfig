a6c33cd18f5502cf8b3aaa6db5b9fcb0564a9407
Synowiec, Kacper <kacper.synowiec@aptiv.com>
2023-05-24 10:28:16 +0200

    json merging

--- generate_workspace.py
+++ generate_workspace.py
@@ -14,4 +14,25 @@ workspace_name = parsed_settings['general']['workspace_name']
 relative_path = f"..\\msw\\{parsed_settings['general']['msw_relative_path']}"
 # %%
 call(["robocopy", f'{file_path}\\.vscode', f'{file_path}\\{relative_path}.vscode'])
-os.rename(f'{relative_path}.vscode\\code-workspace_template.json', f'{relative_path}.vscode\\{workspace_name}.code-workspace')
+
+# %%
+with open(f'{relative_path}.vscode\\code-workspace_template.json') as vsc_workspace_template:
+    vsc_workspace_template_content = vsc_workspace_template.read()
+
+parsed_vsc_workspace_template_content = json.loads(vsc_workspace_template_content)
+# %%
+with open(f'{relative_path}.vscode\\{workspace_name}.code-workspace') as vsc_workspace:
+    vsc_workspace_content = vsc_workspace.read()
+
+parsed_vsc_workspace_content = json.loads(vsc_workspace_content)
+
+# %% MERGE
+for setting in parsed_vsc_workspace_template_content['settings']:
+    parsed_vsc_workspace_content['settings'][setting] = parsed_vsc_workspace_template_content['settings'][setting]
+
+parsed_vsc_workspace_content['extensions'] = parsed_vsc_workspace_template_content['extensions']
+
+
+#%%
+with open(f'{relative_path}.vscode\\{workspace_name}.code-workspace', 'w') as vsc_workspace_write:
+    json.dump(parsed_vsc_workspace_content, vsc_workspace_write)
